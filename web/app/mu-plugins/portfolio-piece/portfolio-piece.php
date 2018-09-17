8772c2eb68dfd4c037aee141541d0e81154149af
Zachary Townsend <zach@verbbrands.com>
2018-09-17 14:37:27 +0200

    Added WP PHP code linting

--- web/app/mu-plugins/portfolio-piece/portfolio-piece.php
+++ web/app/mu-plugins/portfolio-piece/portfolio-piece.php
@@ -13,135 +13,135 @@
 
 // Your code starts here.
 require_once 'vendor/autoload.php';
-// require_once 'extended-cpts/extended-cpts.php';
 
-add_action('init', 'add_portfolio_cpt');
+add_action( 'init', 'add_portfolio_cpt' );
 
 /**
  * Adds Portfolio CPT
  */
 function add_portfolio_cpt() {
-    register_extended_post_type(
-        'project',
-        [
-            'public' => true,
-            'show_in_rest' => true,
-        ],
-        [
-            'singular' => __('Project', 'portfolio-piece'),
-            'plural'   => __('Projects', 'portfolio-piece'),
-            'slug'     => 'project',
-        ]
-    );
+	register_extended_post_type(
+		'project',
+		[
+			'public'       => true,
+			'show_in_rest' => true,
+		],
+		[
+			'singular' => __( 'Project', 'portfolio-piece' ),
+			'plural'   => __( 'Projects', 'portfolio-piece' ),
+			'slug'     => 'project',
+		]
+	);
 
-    register_extended_taxonomy(
-        'project-client',
-        'project',
-        [
-            'hierarchical' => false,
-        ],
-        [
-            'singular' => __('Client', 'portfolio-piece'),
-            'plural'   => __('Clients', 'portfolio-piece'),
-            'slug'     => 'project-client',
-        ]
-    );
+	register_extended_taxonomy(
+		'project-client',
+		'project',
+		[
+			'hierarchical' => false,
+		],
+		[
+			'singular' => __( 'Client', 'portfolio-piece' ),
+			'plural'   => __( 'Clients', 'portfolio-piece' ),
+			'slug'     => 'project-client',
+		]
+	);
 
-    register_extended_taxonomy(
-        'project-type',
-        'project',
-        [
-            'hierarchical' => false,
-        ],
-        [
-        'singular' => __('Project Type', 'portfolio-piece'),
-        'plural'   => __('Project Types', 'portfolio-piece'),
-        'slug'     => 'project-type',
-        ]
-    );
+	register_extended_taxonomy(
+		'project-type',
+		'project',
+		[
+			'hierarchical' => false,
+		],
+		[
+			'singular' => __( 'Project Type', 'portfolio-piece' ),
+			'plural'   => __( 'Project Types', 'portfolio-piece' ),
+			'slug'     => 'project-type',
+		]
+	);
 
-    register_extended_taxonomy(
-        'project-techonology',
-        'project',
-        [],
-        [
-            'singular' => __('Techonology', 'portfolio-piece'),
-            'plural'   => __('Techonologies', 'portfolio-piece'),
-            'slug'     => 'project-techonology',
-        ]
-    );
+	register_extended_taxonomy(
+		'project-techonology',
+		'project',
+		[],
+		[
+			'singular' => __( 'Techonology', 'portfolio-piece' ),
+			'plural'   => __( 'Techonologies', 'portfolio-piece' ),
+			'slug'     => 'project-techonology',
+		]
+	);
 }
 
-add_action('plugins_loaded', 'add_custom_fields', 10000);
+add_action( 'plugins_loaded', 'add_custom_fields', 10000 );
 
+/**
+ * Doc comment
+ */
 function add_custom_fields() {
-    $project_content = acf_flexible_content(
-        [
-            'name' => 'project-content',
-            'label' => 'Project Content',
-            'button_label' => 'Add a page component',
-            'layouts' => [
-                acf_layout(
-                    [
-                        'name' => 'text-block',
-                        'label' => __('Text Block', 'portfolio-piece'),
-                        'sub_fields' => [
-                            acf_text(
-                                [
-                                    'name' => 'block-title',
-                                    'label' => __('Block Title', 'portfolio-piece'),
-                                ]
-                            ),
-                            acf_wysiwyg(
-                                [
-                                    'name' => 'text-block-content',
-                                    'label' => __('Text Block Content', 'portfolio-piece'),
-                                    'required' => true,
-                                    'media_upload' => true,
-                                ]
-                            ),
-                        ],
-                    ]
-                ),
-                acf_layout(
-                    [
-                        'name' => 'image-block',
-                        'label' => __('Image Block', 'portfolio-piece'),
-                        'sub_fields' => [
-                            acf_image(
-                                [
-                                    'name' => 'image',
-                                    'label' => __('Image', 'portfolio-piece'),
-                                    'min_width' => 1500,
-                                    'preview_size' => 'large',
-                                ]
-                            ),
-                            acf_wysiwyg(
-                                [
-                                    'name' => 'text',
-                                    'label' => __('Text', 'portfolio-piece'),
-                                    'media_upload' => false,
-                                ]
-                            ),
-                        ],
-                    ]
-                ),
-            ],
-        ]
-    );
+	$project_content = acf_flexible_content(
+		[
+			'name'         => 'project-content',
+			'label'        => 'Project Content',
+			'button_label' => 'Add a page component',
+			'layouts'      => [
+				acf_layout(
+					[
+						'name'       => 'text-block',
+						'label'      => __( 'Text Block', 'portfolio-piece' ),
+						'sub_fields' => [
+							acf_text(
+								[
+									'name'  => 'block-title',
+									'label' => __( 'Block Title', 'portfolio-piece' ),
+								]
+							),
+							acf_wysiwyg(
+								[
+									'name'         => 'text-block-content',
+									'label'        => __( 'Text Block Content', 'portfolio-piece' ),
+									'required'     => true,
+									'media_upload' => true,
+								]
+							),
+						],
+					]
+				),
+				acf_layout(
+					[
+						'name'       => 'image-block',
+						'label'      => __( 'Image Block', 'portfolio-piece' ),
+						'sub_fields' => [
+							acf_image(
+								[
+									'name'         => 'image',
+									'label'        => __( 'Image', 'portfolio-piece' ),
+									'min_width'    => 1500,
+									'preview_size' => 'large',
+								]
+							),
+							acf_wysiwyg(
+								[
+									'name'         => 'text',
+									'label'        => __( 'Text', 'portfolio-piece' ),
+									'media_upload' => false,
+								]
+							),
+						],
+					]
+				),
+			],
+		]
+	);
 
-    acf_field_group(
-        [
-            'title' => __('Project Flexible Content', 'portfolio-piece'),
-            'fields' => [$project_content],
-            'style' => 'seamless',
-            'location' => [
-                [
-                    acf_location('post_type', 'project')
-                ],
-            ],
-        ]
-    );
+	acf_field_group(
+		[
+			'title'    => __( 'Project Flexible Content', 'portfolio-piece' ),
+			'fields'   => [ $project_content ],
+			'style'    => 'seamless',
+			'location' => [
+				[
+					acf_location( 'post_type', 'project' ),
+				],
+			],
+		]
+	);
 }
-
-?>
